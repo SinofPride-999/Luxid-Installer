40e104ccdabe58fdd7a971071f3984286d70e62a
SinofPride-999 <jhaycodes999@gmail.com>
2026-01-19 16:07:16 +0000

    feat: version command

--- src/Commands/VersionCommand.php
+++ src/Commands/VersionCommand.php
@@ -5,12 +5,21 @@ declare(strict_types=1);
 namespace Luxid\Installer\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Display Luxid Installer version
  */
 class VersionCommand extends Command
 {
-    //
+    protected static $defaultName = 'version';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('<info>Luxid Installer v0.1.0-dev</info>');
+
+        return Command::SUCCESS;
+    }
 }
 
