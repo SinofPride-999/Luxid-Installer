aca4a806eddc0654e813da618d7505d98a8a792d
SinofPride-999 <jhaycodes999@gmail.com>
2026-01-19 16:09:00 +0000

    feat: new command

--- src/Commands/NewCommand.php
+++ src/Commands/NewCommand.php
@@ -5,12 +5,21 @@ declare(strict_types=1);
 namespace Luxid\Installer\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Create a new Luxid application
  */
 class NewCommand extends Command
 {
-    //
+    protected static $defaultName = 'new';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('<comment>Luxid project creation coming soon...</comment>');
+
+        return Command::SUCCESS;
+    }
 }
 
